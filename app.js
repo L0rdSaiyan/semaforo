0cef6dac690c1d11a9378068896c02e554f321b0
joao victor <thehoodgamer20@gmail.com>
2022-08-20 15:31:07 -0300

    massa

---
+++ app.js
@@ -0,0 +1,19 @@
+function yosha(massa){
+
+    if(massa==2){
+
+        return 'massa 2'
+
+    }else if(massa==1){
+
+        return 'massa 1'
+
+    }
+
+    return massa
+
+}
+
+
+let bruh = yosha(2)
+console.log(bruh)
